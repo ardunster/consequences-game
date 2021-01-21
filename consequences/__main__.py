ae981f16619253be5eec264b3a0ee8451765cf63
Anna Dunster <llama747@gmail.com>
2021-01-21 15:02:06 -0800

    Setup directory structure

---
+++ consequences/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from consequences.common.common import template_load, file_load, file_save
+
+if __name__ == '__main__':
+    # Load welcome window
+    # load files: left side content, right side output
+    # populate buttons: left side new, right side load saved
+    # generate
+    pass
